2d77f336d3d2fd6cfa92f8d07b6dc5c70dfc8128
mellobacon <liz6757@outlook.com>
2021-10-23 03:05:57 -0500

    added webpack to fix iframe but it doesnt work and im too lazy to delete it rn

---
+++ src/webpack.config.ts
@@ -0,0 +1,11 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const config = {
+    node: {
+        __dirname: false,
+        __filename: false
+    }
+}
+
+module.exports = config;
